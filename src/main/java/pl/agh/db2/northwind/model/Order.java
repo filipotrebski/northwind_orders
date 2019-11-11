d21fb3e32ebd61ac7fc7bb75a366bf4a2e0115da
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2019-11-11 17:21:57 +0100

    orders class

--- src/main/java/pl/agh/db2/northwind/model/Order.java
+++ src/main/java/pl/agh/db2/northwind/model/Order.java
@@ -1,4 +1,131 @@
 package pl.agh.db2.northwind.model;
 
 public class Order {
+
+    private int orderID;
+    private Customer customerID;
+    private Employee employeeID;
+    private String orderDate;
+    private String requiredDate;
+    private String shippedDate;
+    private Shippers shipVia;
+    private String freight;
+    private String shipName;
+    private String shipAddress;
+    private String shipCity;
+    private String shipRegion;
+    private String shipPostalCode;
+    private String shipCountry;
+
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public Customer getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Customer customerID) {
+        this.customerID = customerID;
+    }
+
+    public Employee getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(Employee employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(String requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    public String getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(String shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public Shippers getShipVia() {
+        return shipVia;
+    }
+
+    public void setShipVia(Shippers shipVia) {
+        this.shipVia = shipVia;
+    }
+
+    public String getFreight() {
+        return freight;
+    }
+
+    public void setFreight(String freight) {
+        this.freight = freight;
+    }
+
+    public String getShipName() {
+        return shipName;
+    }
+
+    public void setShipName(String shipName) {
+        this.shipName = shipName;
+    }
+
+    public String getShipAddress() {
+        return shipAddress;
+    }
+
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+
+    public String getShipCity() {
+        return shipCity;
+    }
+
+    public void setShipCity(String shipCity) {
+        this.shipCity = shipCity;
+    }
+
+    public String getShipRegion() {
+        return shipRegion;
+    }
+
+    public void setShipRegion(String shipRegion) {
+        this.shipRegion = shipRegion;
+    }
+
+    public String getShipPostalCode() {
+        return shipPostalCode;
+    }
+
+    public void setShipPostalCode(String shipPostalCode) {
+        this.shipPostalCode = shipPostalCode;
+    }
+
+    public String getShipCountry() {
+        return shipCountry;
+    }
+
+    public void setShipCountry(String shipCountry) {
+        this.shipCountry = shipCountry;
+    }
 }
