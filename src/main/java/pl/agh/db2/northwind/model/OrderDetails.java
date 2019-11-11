51701f2d7cc8336a29fb6e1aa346f16eba20d2c2
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2019-11-11 17:40:01 +0100

    order details class

--- src/main/java/pl/agh/db2/northwind/model/OrderDetails.java
+++ src/main/java/pl/agh/db2/northwind/model/OrderDetails.java
@@ -1,4 +1,10 @@
 package pl.agh.db2.northwind.model;
 
 public class OrderDetails {
+
+    private int orderID;
+    private Product productID;
+    private float unitPrice;
+    private int quantity;
+    private float discount;
 }
