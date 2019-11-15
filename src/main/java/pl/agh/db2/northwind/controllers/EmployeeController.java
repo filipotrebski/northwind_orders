955a97cb7c7aafd5978a994e6019a05cacebff7e
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2019-11-15 21:11:03 +0100

    update template controllers

--- src/main/java/pl/agh/db2/northwind/controllers/EmployeeController.java
+++ src/main/java/pl/agh/db2/northwind/controllers/EmployeeController.java
@@ -1,7 +1,16 @@
 package pl.agh.db2.northwind.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EmployeeController {
+
+    @GetMapping("/employee")
+    @ResponseBody
+    public String employee(){
+
+        return "employee";
+    }
 }
