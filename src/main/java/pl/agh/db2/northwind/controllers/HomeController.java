e67bc867afbd750a1f3d9a9c4ebf3d5b445f9704
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2019-11-16 12:26:32 +0100

    Rest Controller

--- src/main/java/pl/agh/db2/northwind/controllers/HomeController.java
+++ src/main/java/pl/agh/db2/northwind/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package pl.agh.db2.northwind.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
