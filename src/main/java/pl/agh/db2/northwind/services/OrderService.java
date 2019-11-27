f9cf7fef7fac68aca8452d5a3b0043733177ebf1
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2019-11-27 21:33:44 +0100

    add autowired to class services

--- src/main/java/pl/agh/db2/northwind/services/OrderService.java
+++ src/main/java/pl/agh/db2/northwind/services/OrderService.java
@@ -1,6 +1,12 @@
 package pl.agh.db2.northwind.services;
 
+
 import pl.agh.db2.northwind.repository.OrderDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 public class OrderService {
+
+    @Autowired
+    private OrderDao orderDeo;
 }
