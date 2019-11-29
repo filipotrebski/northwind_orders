132db7e755dc6fd0e0506d09a9669d90419a3918
filipotrebski <filo1111.fo@gmail.com>
2019-11-29 20:30:02 +0100

    Fixed tests

--- src/test/java/pl/agh/db2/northwind/NorthwindApplicationTests.java
+++ src/test/java/pl/agh/db2/northwind/NorthwindApplicationTests.java
@@ -2,7 +2,9 @@ package pl.agh.db2.northwind;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:application-test.properties")
 @SpringBootTest
 class NorthwindApplicationTests {
 
