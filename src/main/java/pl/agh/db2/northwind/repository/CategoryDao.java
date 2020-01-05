1ef96c9854c0a6dcbc66ed1ed2f3cc9e4c49c020
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-01-05 16:40:34 +0100

    categoryDeo

--- src/main/java/pl/agh/db2/northwind/repository/CategoryDao.java
+++ src/main/java/pl/agh/db2/northwind/repository/CategoryDao.java
@@ -1,12 +1,25 @@
 package pl.agh.db2.northwind.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.agh.db2.northwind.model.Category;
 
+import java.util.List;
+
 @Transactional
 @Repository
-public interface CategoryDao extends CrudRepository<Category,Integer> {
+public interface CategoryDao extends JpaRepository<Category,Integer> {
 
+    @Query(value = "SELECT c FROM Category c")
+    public List<Category> showAllCategory();
+
+    @Override
+    public Category save(Category c);
+
+    @Override
+    public void delete(Category c);
 }
