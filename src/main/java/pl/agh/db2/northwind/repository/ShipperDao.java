dbe68ecedb1ea8fa8e19c70489feaff8faff31c1
hubertmakula <hubertmakula@MacBook-Air-hubertmakula.local>
2020-01-06 17:22:59 +0100

    Supplier repository, Shipper repository

--- src/main/java/pl/agh/db2/northwind/repository/ShipperDao.java
+++ src/main/java/pl/agh/db2/northwind/repository/ShipperDao.java
@@ -1,10 +1,12 @@
 package pl.agh.db2.northwind.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.agh.db2.northwind.model.Shipper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -13,4 +15,13 @@ public interface ShipperDao extends CrudRepository<Shipper, Integer> {
 
     @Override
     Optional<Shipper> findById(Integer integer);
+
+    @Query(value = "SELECT s FROM Shipper s")
+    public List<Shipper> showAllShipper();
+
+    @Override
+    public Shipper save(Shipper s);
+
+    @Override
+    public void delete(Shipper s);
 }
--- src/main/java/pl/agh/db2/northwind/repository/SupplierDao.java
+++ src/main/java/pl/agh/db2/northwind/repository/SupplierDao.java
@@ -1,10 +1,12 @@
 package pl.agh.db2.northwind.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.agh.db2.northwind.model.Supplier;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -13,4 +15,13 @@ public interface SupplierDao extends CrudRepository<Supplier, Integer> {
 
     @Override
     Optional<Supplier> findById(Integer integer);
+
+    @Query(value = "SELECT s FROM Supplier s")
+    public List<Supplier> showAllSuppliers();
+
+    @Override
+    public Supplier save(Supplier s);
+
+    @Override
+    public void delete(Supplier s);
 }
