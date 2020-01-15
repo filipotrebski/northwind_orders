13bb8b67efc6cad1e8980f4b0d40c3622999fdbb
dawpietr <dawpietr@gmail.com>
2020-01-15 13:38:15 -0800

    SupplierMapper

---
+++ src/main/java/pl/agh/db2/northwind/mapper/SupplierMapper.java
@@ -0,0 +1,64 @@
+package pl.agh.db2.northwind.mapper;
+
+import org.springframework.stereotype.Component;
+import pl.agh.db2.northwind.model.Supplier;
+import pl.agh.db2.northwind.model.SupplierDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Component
+public class SupplierMapper {
+
+    public Supplier mapToSupplier(final SupplierDto supplierDto){
+        return new Supplier(
+                supplierDto.getSupplierID(),
+                supplierDto.getCompanyName(),
+                supplierDto.getContactName(),
+                supplierDto.getContactTitle(),
+                supplierDto.getAddress(),
+                supplierDto.getCity(),
+                supplierDto.getRegion(),
+                supplierDto.getPostalCode(),
+                supplierDto.getCountry(),
+                supplierDto.getPhone(),
+                supplierDto.getFax(),
+                supplierDto.getHomePage()
+        );
+    }
+    public SupplierDto mapToCategoryDto(final Supplier supplier) {
+        return new SupplierDto(
+                supplier.getSupplierID(),
+                supplier.getCompanyName(),
+                supplier.getContactName(),
+                supplier.getContactTitle(),
+                supplier.getAddress(),
+                supplier.getCity(),
+                supplier.getRegion(),
+                supplier.getPostalCode(),
+                supplier.getCountry(),
+                supplier.getPhone(),
+                supplier.getFax(),
+                supplier.getHomePage()
+        );
+    }
+
+    public List<SupplierDto> mapToSupplierDtoList(final List<Supplier> supplierList) {
+        return supplierList.stream()
+                .map( t -> new SupplierDto(t.getSupplierID(),
+                        t.getCompanyName(),
+                        t.getContactName(),
+                        t.getContactTitle(),
+                        t.getAddress(),
+                        t.getCity(),
+                        t.getRegion(),
+                        t.getPostalCode(),
+                        t.getCountry(),
+                        t.getPhone(),
+                        t.getFax(),
+                        t.getHomePage()))
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/pl/agh/db2/northwind/model/SupplierDto.java
@@ -0,0 +1,23 @@
+package pl.agh.db2.northwind.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class SupplierDto {
+    private Integer supplierID;
+    private String companyName;
+    private String contactName;
+    private String contactTitle;
+    private String address;
+    private String city;
+    private String region;
+    private String postalCode;
+    private String country;
+    private String phone;
+    private String fax;
+    private String homePage;
+}
