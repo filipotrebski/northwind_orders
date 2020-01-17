ad46532c680cc3911ed4b5900a49b3f541f06e20
filipotrebski <filo1111.fo@gmail.com>
2020-01-17 23:25:24 +0100

    cart added, helper added

--- src/main/java/pl/agh/db2/northwind/model/OrderDetails.java
+++ src/main/java/pl/agh/db2/northwind/model/OrderDetails.java
@@ -1,72 +1,37 @@
 package pl.agh.db2.northwind.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @IdClass(OrderDetailsId.class)
 @Table(name = "ORDERDETAILS")
 public class OrderDetails {
 
+    @Id
     private Integer orderId;
+    @Id
     private Integer productId;
-    private float unitPrice;
+    private double unitPrice;
     private int quantity;
     private float discount;
 
-    public OrderDetails() {
-    }
-
-    public OrderDetails(Integer orderId, Integer productId, float unitPrice, int quantity, float discount) {
-        this.orderId = orderId;
-        this.productId = productId;
-        this.unitPrice = unitPrice;
-        this.quantity = quantity;
-        this.discount = discount;
-    }
-
-    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "ORDERID", referencedColumnName = "orderId")
-    public Integer getOrderId() {
-        return orderId;
-    }
-
-    @Id
-//    @ManyToOne
-//    @JoinColumn (name = "PRODUCTID", referencedColumnName = "productId")
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public float getUnitPrice() {
-        return unitPrice;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public float getDiscount() {
-        return discount;
-    }
-
-    public void setOrderId(Integer orderId) {
-        this.orderId = orderId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public void setUnitPrice(float unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setDiscount(float discount) {
-        this.discount = discount;
-    }
+//    public OrderDetails() {
+//    }
+//
+//    public OrderDetails(Integer orderId, Integer productId, float unitPrice, int quantity, float discount) {
+//        this.orderId = orderId;
+//        this.productId = productId;
+//        this.unitPrice = unitPrice;
+//        this.quantity = quantity;
+//        this.discount = discount;
+//    }
 }
