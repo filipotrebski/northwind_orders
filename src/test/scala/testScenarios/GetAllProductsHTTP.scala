195013443e9f05fe2d0bcb8b0732f99c12906e58
filipotrebski <filo1111.fo@gmail.com>
2020-01-18 12:37:51 +0100

    get all employyes Ramp users

--- src/test/scala/testScenarios/GetAllProductsHTTP.scala
+++ src/test/scala/testScenarios/GetAllProductsHTTP.scala
@@ -1,25 +1,21 @@
 package testScenarios
 
-import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
-
-import scala.util.Random
+import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
-import scala.language.postfixOps
 
-class DeleteProductTestHTTP extends Simulation{
-
+class GetAllProductsHTTP extends  Simulation{
   val httpConf = http.baseUrl("http://localhost:8080/v1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("delete product from database with atOnce")
+  val scn = scenario("Get product from database with atOnce")
     .repeat(100) {
-      exec(http("delete product").delete(s"/product/${Random.nextInt(1000)+1}"))
+      exec(http("getProduct").get(s"/product"))
         .pause(10 millis)
     }
 
