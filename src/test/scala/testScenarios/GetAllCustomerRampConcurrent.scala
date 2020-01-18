cd3ea0dbca7ba99722d08deefd167bc2c8882c3f
filipotrebski <filo1111.fo@gmail.com>
2020-01-18 13:20:53 +0100

    all customers ramp conncurent users

--- src/test/scala/testScenarios/GetAllCustomerRampConcurrent.scala
+++ src/test/scala/testScenarios/GetAllCustomerRampConcurrent.scala
@@ -1,29 +1,26 @@
 package testScenarios
 
-import io.gatling.core.scenario.Simulation
 import io.gatling.core.Predef._
-
-import scala.util.Random
+import io.gatling.core.scenario.Simulation
 import io.gatling.http.Predef._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-
-class GetProductHTTP extends  Simulation {
-
+class GetAllCustomerRampConcurrent extends Simulation{
   val httpConf = http.baseUrl("http://localhost:8080/v1")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("Get product from database with atOnce")
+  val scn = scenario("Get all customers with rampConcurrent user")
     .repeat(100) {
-      exec(http("getProduct").get(s"/product/{id}?id=${Random.nextInt(1000)+1}"))
+      exec(http("Get All Customers").get(s"/customer"))
         .pause(10 millis)
     }
 
   setUp(
-    scn.inject(atOnceUsers(50))
+    scn.inject(constantConcurrentUsers(10) during (10 seconds),
+      rampConcurrentUsers(10) to (20) during (10 seconds))
   ).protocols(httpConf)
 }
